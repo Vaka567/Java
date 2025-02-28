79091f2d324427ee612b7c227a7933b9db72fcbf
sriharithavaka <sriharithavaka0401@gmail.com>
2025-02-27 17:39:57 -0800

    commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = 1;
+        switch(n) {
+            case 1 :
+                System.out.println(a+b);
+                break;
+            case 2 :
+                System.out.println(a-b);
+                break;
+            case 3 :
+                System.out.println(a*b);
+                break;
+            case 4 :
+                System.out.println(a/b);
+                break;
+            case 5 :
+                System.out.println(a%b);
+                break;
+            default :
+                System.out.println("not valid");
+
+
+        }
+
+    }
+}
