4a63d0bee4aeecc7925bd64b434d82841d909296
sriharithavaka <sriharithavaka0401@gmail.com>
2025-03-25 18:45:50 -0700

    printing rectangles

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,28 @@
-import java.sql.SQLOutput;
 import java.util.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class Main {
+public class Main {
+//printing rectangles
+    public static void bruteForcePrint() {
+        for (int i = 0; i < 20; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 18; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
+
+        for (int i = 0; i < 20; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int n = 1;
-        for(int i = 0;i<10;i++ ){
-            System.out.println(n*i);
-        }
-        switch(n) {
-            case 1 :
-                System.out.println(a+b);
-                break;
-            case 2 :
-                System.out.println(a-b);
-                break;
-            case 3 :
-                System.out.println(a*b);
-                break;
-            case 4 :
-                System.out.println(a/b);
-                break;
-            case 5 :
-                System.out.println(a%b);
-                break;
-            default :
-                System.out.println("not valid");
-
-
-        }
-
+        bruteForcePrint();
     }
 }
+
